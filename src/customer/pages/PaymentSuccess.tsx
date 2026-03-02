9999ea8afb2b3b1da8bd40518935d3bb6ed47782
Prakhar <prakharjoshi960@gmail.com>
2026-03-02 18:48:55 +0530

    Updates

--- src/customer/pages/PaymentSuccess.tsx
+++ src/customer/pages/PaymentSuccess.tsx
@@ -24,7 +24,7 @@ const PaymentSuccess = () => {
   return (
     <div className='min-h-[90vh] flex justify-center items-center'>
         <div className='bg-primary-color text-white p-8 w-[90%] lg:w-[25%] border rounded-md h-[40vh] flex flex-col gap-7 items-center justify-center'>
-            <h1 className='text-3xl font-semibold'>Congratulation</h1>
+            <h1 className='text-3xl font-semibold'>Congratulations!!!</h1>
             <h1 className='text-2xl font-semibold'>your order get success</h1>
 
             <div>
